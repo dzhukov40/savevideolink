2365ab86f1fba877fea66fca322af879100369b2
dzhukov40 <dzhukov40.v@mail.ru>
2017-11-10 01:13:54 +0300

    разбираемся с liqubase + добавили скрипты для наката и удаления тестовых данных

--- db/liquibase/testData/test_delete_1.0.0.sql
+++ db/liquibase/testData/test_delete_1.0.0.sql
@@ -1,1 +1,19 @@
 -- Это скрипт удаления тестовых данных
+
+-- [ links_lists ]
+DELETE FROM public.links_lists;
+
+-- [ lists ]
+DELETE FROM public.lists;
+
+-- [ links ]
+DELETE FROM public.links;
+
+-- [ all_users_roles ]
+DELETE FROM public.all_users_roles;
+
+-- [ roles ]
+DELETE FROM public.roles;
+
+-- [ all_users ]
+DELETE FROM public.all_users;
--- db/liquibase/testData/test_insert_1.0.0.sql
+++ db/liquibase/testData/test_insert_1.0.0.sql
@@ -1,8 +1,34 @@
 -- Это скрипт наката тестовых данных
 
-INSERT INTO public.all_users(name, password) VALUES ('Den', 'Den123');
-INSERT INTO public.all_users(name, password) VALUES ('Omar', 'Omar123');
-INSERT INTO public.all_users(name, password) VALUES ('Abraham', 'Abraham123');
-INSERT INTO public.all_users(name, password) VALUES ('Hamilton', 'Hamilton123');
-INSERT INTO public.all_users(name, password) VALUES ('Omar', 'Omar123');
-INSERT INTO public.all_users(name, password) VALUES ('Valarie', 'Valarie123');
+-- [ all_users ]
+INSERT INTO public.all_users(user_id, name, password) VALUES (1, 'Den', 'Den123');
+INSERT INTO public.all_users(user_id, name, password) VALUES (2, 'Omar', 'Omar123');
+INSERT INTO public.all_users(user_id, name, password) VALUES (3, 'Abraham', 'Abraham123');
+INSERT INTO public.all_users(user_id, name, password) VALUES (4, 'Hamilton', 'Hamilton123');
+INSERT INTO public.all_users(user_id, name, password) VALUES (5, 'Valarie', 'Valarie123');
+
+-- [ roles ]
+-- ROLE_ANONYMOUS : 1
+-- ROLE_USER      : 2
+-- ROLE_ADMIN     : 3
+INSERT INTO public.roles(role_id, role) VALUES (1, 1);
+INSERT INTO public.roles(role_id, role) VALUES (2, 2);
+INSERT INTO public.roles(role_id, role) VALUES (3, 3);
+
+-- [ all_users_roles ]
+INSERT INTO public.all_users_roles(user_id, role_id) VALUES (1, 1);
+INSERT INTO public.all_users_roles(user_id, role_id) VALUES (2, 2);
+INSERT INTO public.all_users_roles(user_id, role_id) VALUES (3, 3);
+
+-- [ links ]
+INSERT INTO public.links(link_id, link, description, link_master) VALUES (1, 'https://youtu.be/7CtJUTJ0wjE?list=PL7HGoJDlGoPz-aOqAOLoT8UOi2FSdw-4R', 'король мортимер',1);
+INSERT INTO public.links(link_id, link, description, link_master) VALUES (2, 'https://vk.com/video-57846937_456247043', 'жиза', 2);
+INSERT INTO public.links(link_id, link, description, link_master) VALUES (3, 'http://hdgo.cc/video/t/tsl0e90bjebb6b7retuw12hp/352173/', 'друзья', 3);
+
+-- [ lists ]
+INSERT INTO public.lists(description, list_master) VALUES ('список фильмецов', 1);
+
+-- [ links_lists ]
+INSERT INTO public.links_lists(link_id, list_id) VALUES (1, 1);
+INSERT INTO public.links_lists(link_id, list_id) VALUES (2, 1);
+INSERT INTO public.links_lists(link_id, list_id) VALUES (3, 1);
